83ed05f431516a0b0aa7590ab68f1749d3713d91
Kendall Hopkins <softwareelves@gmail.com>
2012-07-15 17:26:26 -0400

    Change build script to use cleancss and uglifyjs.

--- build.command
+++ build.command
@@ -1,4 +1,15 @@
 #!/bin/bash
 cd "`dirname "$0"`"
-coffee -c ./main.coffee
-lessc main.less main.css
+
+{
+	cat vendor/fancyBox/source/jquery.fancybox.css;
+	cat vendor/jQuery-contextMenu/src/jquery.contextMenu.css;
+	lessc main.less;
+} | sed 's/\/\*\!/\/\*/g' | cleancss -o ./build.css
+
+{
+	cat vendor/jsPlumb/build/1.3.9/js/jquery.jsPlumb-1.3.9-all-min.js;
+	cat vendor/fancyBox/source/jquery.fancybox.js;
+	cat vendor/jQuery-contextMenu/src/jquery.contextMenu.js;
+	coffee -p -c ./main.coffee;
+} | uglifyjs -o ./build.js
--- index.php
+++ index.php
@@ -4,26 +4,25 @@
 		<meta charset="utf-8"/>
 		<title>Regex Engine</title>
 		<link rel="stylesheet" href="//current.bootstrapcdn.com/bootstrap-v204/css/bootstrap.min.css"/>
+<? if( array_key_exists( "debug", $_GET ) ) { ?>
 		<link rel="stylesheet" href="vendor/fancyBox/source/jquery.fancybox.css"/>
 		<link rel="stylesheet" href="vendor/jQuery-contextMenu/src/jquery.contextMenu.css"/>
-<? if( array_key_exists( "debug", $_GET ) ) { ?>
 		<link rel="stylesheet/less" href="main.less">
 <? } else { ?>
-		<link rel="stylesheet" href="main.css"/>
+		<link rel="stylesheet" href="build.css?_t=<? print filemtime( __DIR__."/build.css" ) ?>"/>
 <? } ?>
-		<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js'></script>
-		<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.js'></script>
-		<script type='text/javascript' src='//current.bootstrapcdn.com/bootstrap-v204/js/bootstrap.min.js'></script>
-		<script type='text/javascript' src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/r29/html5.js"></script>
-		<script type='text/javascript' src='vendor/jsPlumb/build/1.3.9/js/jquery.jsPlumb-1.3.9-all-min.js'></script>
-		<script type='text/javascript' src='vendor/fancyBox/source/jquery.fancybox.js'></script>
-		<script type='text/javascript' src='vendor/jQuery-contextMenu/src/jquery.contextMenu.js'></script>
+		<script type='text/javascript' defer="defer" src='//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js'></script>
+		<script type='text/javascript' defer="defer" src='//ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.js'></script>
+		<script type='text/javascript' defer="defer" src='//current.bootstrapcdn.com/bootstrap-v204/js/bootstrap.min.js'></script>
 <? if( array_key_exists( "debug", $_GET ) ) { ?>
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.3.0/less-1.3.0.min.js"></script>
+		<script type='text/javascript' defer="defer" src='vendor/jsPlumb/build/1.3.9/js/jquery.jsPlumb-1.3.9-all-min.js'></script>
+		<script type='text/javascript' defer="defer" src='vendor/fancyBox/source/jquery.fancybox.js'></script>
+		<script type='text/javascript' defer="defer" src='vendor/jQuery-contextMenu/src/jquery.contextMenu.js'></script>
+		<script type="text/javascript" defer="defer" src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.3.0/less-1.3.0.min.js"></script>
 		<script type="text/coffeescript" src="main.coffee"></script>
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/coffee-script/1.3.1/coffee-script.min.js"></script>
+		<script type="text/javascript" defer="defer" src="//cdnjs.cloudflare.com/ajax/libs/coffee-script/1.3.1/coffee-script.min.js"></script>
 <? } else { ?>
-		<script type='text/javascript' src='main.js'></script>
+		<script type='text/javascript' defer="defer" src='build.js?_t=<? print filemtime( __DIR__."/build.js" ) ?>'></script>
 <? } ?>
 	</head>
 	<body onunload="jsPlumb.unload();">	
